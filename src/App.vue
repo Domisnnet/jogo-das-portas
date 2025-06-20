eb48bb782ab6730e86c27942f633e0b58f042802
Domisnnet <dominique.goncalves61@gmail.com>
2025-06-19 21:42:20 -0300

    alterado

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,49 @@
 <template>
   <div id="app">
-    <h1>Welcome to Monty</h1>
+    <h1>Bem Vindo ao Jogo das Portas!!!</h1>
+    <Gift />
   </div>
 </template>
 
 <script>
+  import Gift from "./components/Gift.vue";
 
+export default {
+  name: "App",
+  components: { Gift },
+}
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
+}
 
+body {
+  color: #fff;
+  background: linear-gradient(to top, #240b36, #c31432) no-repeat;
+  height: 100vh;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#app h1 {
+  width: 580px;
+  border: 1px solid #000;
+  background-color: #0004;
+  padding: 20px;
+  margin-bottom: 60px;
+  border-radius: 15px;
+}
+
+#app p {
+  font-size: 4rem;
+  text-align: center;
+  margin: 0; 
+}
 </style>
---
+++ src/components/Gift.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="gift">
+        <p>🎁</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GiftComponent",
+};
+</script>
+
+<style>
+.gift {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    z-index: 10;
+}
+</style>
